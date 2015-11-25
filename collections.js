90d80e8e75a37cac3e4cac137a5ab7ea1282e684
Clement Bourgoin <cb@nokto.net>
2015-11-25 13:24:35 +0100

    Rename (and improve) message collection and schema to cards

--- collections.js
+++ collections.js
@@ -1,8 +1,43 @@
 "use strict";
 
-Nbsp.Messages = new Mongo.Collection("rooms");
+Nbsp.Cards = new Mongo.Collection("cards");
 
-Nbsp.MessageSchema = new SimpleSchema({
+Nbsp.Cards.allow({
+  insert() { return false; },
+  update() { return false; },
+  remove() { return false; }
+});
+
+Nbsp.Cards.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; }
+});
+
+Nbsp.CardPositionSchema = new SimpleSchema({
+  x: {
+    type: Number,
+    min: 0,
+    max: 100
+  },
+  y: {
+    type: Number,
+    min: 0,
+    max: 100
+  },
+  z: {
+    type: Number,
+    min: 1,
+    max: 9999
+  },
+  rotation: {
+    type: Number,
+    min: 0,
+    max: 100
+  },
+});
+
+Nbsp.CardSchema = new SimpleSchema({
   from: {
     type: String,
     label: "Your name",
@@ -10,8 +45,17 @@ Nbsp.MessageSchema = new SimpleSchema({
   },
   content: {
     type: String,
-    label: "Your message"
+    label: "Your message",
     max: 256
+  },
+  hue: {
+    type: Number,
+    label: "Light color",
+    min: 0,
+    max: 65535
+  },
+  position: {
+    type: Nbsp.CardPositionSchema
   },
   createdAt: {
     type: Date
